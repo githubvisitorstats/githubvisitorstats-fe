8e11724360dea4226834e926db409e5a1d94f31f
Yasin KALKAN <yasinburakkalkan@gmail.com>
2024-10-27 18:57:58 +0300

    [fix] Analytics fixed.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
 import "../styles/style.css";
 
+import theme from "@/theme";
+import Script from "next/script";
+
+import { ThemeProvider } from "@emotion/react";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
