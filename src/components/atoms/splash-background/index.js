d80ad5984ebe5e653cf41a52af98cb2b273065f3
Yasin KALKAN <yasinburakkalkan@gmail.com>
2024-10-30 22:13:16 +0300

    [feat] Splash background component development.

--- src/components/atoms/splash-background/index.js
+++ src/components/atoms/splash-background/index.js
@@ -1,7 +1,14 @@
+import theme from "@/theme";
 import { Stack } from "@mui/material";
 
 const SplashBackground = () => {
-  return <Stack></Stack>;
+  return (
+    <Stack
+      sx={{
+        backgroundImage: `radial-gradient(circle at 50% 50%, ${theme.palette.purple.main}, ${theme.palette.black.main} 87%)`,
+      }}
+    ></Stack>
+  );
 };
 
 export default SplashBackground;
