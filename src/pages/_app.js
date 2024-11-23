cade0832962a3c3c021c69221bc16ba3e719e968
Yasin KALKAN <yasinburakkalkan@gmail.com>
2024-11-23 08:49:37 +0300

    [fix] Page load optimization removed.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,10 +6,6 @@ import { ThemeProvider } from "@emotion/react";
 import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    document.body.classList.add("page-loaded");
-  }, []);
-
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
